48c00ab9976fd44e24e341b2a23af55390c91df1
Motaly <mohamedhenvalleladil@gmail.com>
2025-04-22 01:29:32 +0000

    debut du projet

---
+++ android/app/src/main/kotlin/com/example/projet_soutenance/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projet_soutenance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HR',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(144, 9, 118, 213)),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
